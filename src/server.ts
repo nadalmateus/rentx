baebd790f3de28789d972ee2bcc7559d2a480c5c
Mateus Nadal <nadalmateus@outlook.com>
2021-03-15 22:50:15 -0300

    feat(routes): creating category

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 app.use(express.json());
+
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("🚀 Server running on port 3333!"));
