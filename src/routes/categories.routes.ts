98abd6b1afc7cde1a495b6598623f3a98732d444
Mateus Nadal <nadalmateus@outlook.com>
2021-03-16 10:48:13 -0300

    feat(categories): creating categories and entering category typing
    Creating categories and entering category typing and add uuidv4

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,25 @@
 import { Router } from "express";
+import { v4 as uuidv4 } from "uuid";
+
+import Category from "../model/Category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
+  const category = new Category();
+  Object.assign(category, {
     name,
     description,
+    created_at: new Date(),
   });
 
-  return response.status(201).send();
+  categories.push(category);
+
+  return response.status(201).json({ category });
 });
 
 export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use(categoriesRoutes);
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("🚀 Server running on port 3333!"));
