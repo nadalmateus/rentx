215a84c94bab8970526e306850ce4e4489938d7c
Mateus Nadal <nadalmateus@outlook.com>
2021-03-16 21:31:26 -0300

    feat(categories): validating the category registration
    Validating the category registration and creating repositories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,28 @@
 import { Router } from "express";
-import { v4 as uuidv4 } from "uuid";
 
-import Category from "../model/Category";
+import CategoriesRepository from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-  categories.push(category);
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ message: "Category already exists!" });
+  }
 
-  return response.status(201).json({ category });
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
