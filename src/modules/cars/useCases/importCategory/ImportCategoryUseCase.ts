2e3a8f92d7002fc92c694a64f327c0065ddf52ba
Mateus Nadal <nadalmateus@outlook.com>
2021-03-20 19:36:32 -0300

    feat(stream): read files with stream

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,19 @@
+import csvParse from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
-  execute(file: any) {
+  execute(file: Express.Multer.File): void {
     console.log(file);
+
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
