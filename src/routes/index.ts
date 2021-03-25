9b7b76e57d3790cf9b39e88d7f34a5e519a0355d
Mateus Nadal <nadalmateus@outlook.com>
2021-03-25 11:28:30 -0300

    Readme fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { categoriesRoutes } from "./categories.routes";
-import { specificationsRoutes } from "./spcecifications.routes";
+import { specificationsRoutes } from "./specifications.routes";
 
 const router = Router();
 
