6c867bf3c36beca41f875785efcbf92bc96303d8
Mateus Nadal <nadalmateus@outlook.com>
2021-04-06 11:04:49 -0300

    fix(changing the category repository): changing the category repository

--- src/modules/cars/repositories/implementetions/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementetions/CategoriesRepository.ts
@@ -1,16 +1,18 @@
-import Category from "../../model/Category";
+import { getRepository, Repository } from "typeorm";
+
+import Category from "../../entities/Category";
 import {
     ICategoriesRepository,
     ICreateCategoryDTO,
 } from "../ICategoriesRepository";
 
 class CategoriesRepository implements ICategoriesRepository {
-    private categories: Category[];
+    private repository: Repository<Category>;
 
     private static INSTANCE: CategoriesRepository;
 
     private constructor() {
-        this.categories = [];
+        this.repository = getRepository(Category);
     }
 
     public static getInstance(): CategoriesRepository {
@@ -21,25 +23,21 @@ class CategoriesRepository implements ICategoriesRepository {
         return CategoriesRepository.INSTANCE;
     }
 
-    create({ name, description }: ICreateCategoryDTO): void {
-        const category = new Category();
-
-        Object.assign(category, {
-            name,
+    async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+        const category = this.repository.create({
             description,
-            created_at: new Date(),
+            name,
         });
 
-        this.categories.push(category);
+        await this.repository.save(category);
     }
-    list(): Category[] {
-        return this.categories;
+    async list(): Promise<Category[]> {
+        const categories = await this.repository.find();
+        return categories;
     }
 
-    findByName(name: string): Category {
-        const category = this.categories.find(
-            (category) => category.name === name
-        );
+    async findByName(name: string): Promise<Category> {
+        const category = await this.repository.findOne({ name });
 
         return category;
     }
