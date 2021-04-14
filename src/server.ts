e0eaa6d31acdfb79d7a5bf518ddb32220371d514
Mateus Nadal <nadalmateus@outlook.com>
2021-04-14 19:13:34 -0300

    feat(reestarting project): initial commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.send('Hello World')
+})
+
+const port = 3333
+app.listen(port, () => console.log(`Server running on ${port}!`))
