3c93f1815b6a974a85147749c8507dcb54799c41
Mateus Nadal <nadalmateus@outlook.com>
2021-04-16 15:30:01 -0300

    feat(inserting typing for category): inserting typing for category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,25 @@
 import { Router } from "express";
-import { v4 as uuidv4 } from "uuid";
+
+import { Category } from "../model/Category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
+  const category = new Category();
+
+  Object.assign(category, {
     name,
     description,
-    id: uuidv4(),
-  };
+    created_at: new Date(),
+  });
 
   categories.push(category);
 
-  console.log(categories);
-
-  return response.status(201).send();
+  return response.status(201).json({ category });
 });
 
 export { categoriesRoutes };
