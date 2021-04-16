f0cfe4f2bcb75ca284fe0379751388a65e0a62cc
Mateus Nadal <nadalmateus@outlook.com>
2021-04-16 16:18:52 -0300

    feat(categories): validating the category registration
    Validating the category registration

--- src/repositories/CategoriesRepositories.ts
+++ src/repositories/CategoriesRepositories.ts
@@ -23,6 +23,14 @@ class CategoriesRepository {
 
     this.categories.push(category);
   }
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
+  }
 }
 
 export { CategoriesRepository };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,9 +8,21 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ message: "Category already exists!" });
+  }
+
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json({ all });
 });
 
 export { categoriesRoutes };
