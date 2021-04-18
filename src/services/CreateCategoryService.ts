a2a79b7219e8d3b100acfb61fd476d15553114c8
Mateus Nadal <nadalmateus@outlook.com>
2021-04-17 21:32:35 -0300

    refactor(solid): using the Principle of Single Responsibility (SRP)
    Using the Principle of Single Responsibility (SRP)

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,22 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepositories";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryService {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error("Category already exists!");
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService };
