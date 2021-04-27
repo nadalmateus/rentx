c1aae74438a9cf5a8d07394fd5292c6d63e09c0a
Mateus Nadal <nadalmateus@outlook.com>
2021-04-27 11:43:26 -0300

    docs(postman collection): postman collection

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -8,7 +8,7 @@ class ListCategoriesController {
   handle (request: Request, response: Response): Response {
     const all = this.listCategoriesUseCase.execute()
 
-    return response.json({ all })
+    return response.json(all)
   }
 }
 
