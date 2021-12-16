e945d241dea06d6cb2a41ea251c7688f10250c68
Mateus Nadal <nadalmateus@hotmail.com>
2021-12-16 14:44:34 -0300

    docs: added swagger ui

--- source/modules/cars/use-cases/categories.use-cases/import-category/import-category.index.ts
+++
@@ -1,7 +0,0 @@
-import { ImportCategoryController } from './import-category.controller'
-import { ImportCategoryUseCase } from './import-category.use-case'
-
-const importCategoryUseCase = new ImportCategoryUseCase()
-const importCategoryController = new ImportCategoryController(importCategoryUseCase)
-
-export { importCategoryController }
--- source/routes/categories.routes.ts
+++ source/routes/categories.routes.ts
@@ -1,14 +1,8 @@
 import { Router } from 'express'
-import multer from 'multer'
 import { createCategoryController } from '../modules/cars/use-cases/categories.use-cases/create-category/create-category.index'
-import { importCategoryController } from '../modules/cars/use-cases/categories.use-cases/import-category/import-category.index'
 import { listCategoryController } from '../modules/cars/use-cases/categories.use-cases/list-category/list-category.index'
 
 const categoriesRoutes = Router()
-
-const upload = multer({
-  dest: './temp'
-})
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response)
@@ -16,10 +10,6 @@ categoriesRoutes.post('/', (request, response) => {
 
 categoriesRoutes.get('/', async (request, response) => {
   return listCategoryController.handle(request, response)
-})
-
-categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
-  return importCategoryController.handle(request, response)
 })
 
 export { categoriesRoutes }
