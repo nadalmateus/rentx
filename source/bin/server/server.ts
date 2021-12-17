7e6710427eb920c781dff7f314377b7abf08b288
Mateus Nadal <nadalmateus@hotmail.com>
2021-12-17 18:34:21 -0300

    fix: linter fix

--- source/bin/server/server.ts
+++ source/bin/server/server.ts
@@ -2,8 +2,8 @@ import express from 'express'
 import helmet from 'helmet'
 import compression from 'compression'
 import { indexRoutes } from '../../routes/index.routes'
-import swaggerUi from 'swagger-ui-express'
-import swaggerFile from '../server/swagger.json'
+// import swaggerUi from 'swagger-ui-express'
+// import swaggerFile from '../server/swagger.json'
 
 const server = express()
 
@@ -11,7 +11,7 @@ server.use(helmet())
 server.use(compression())
 server.use(express.json())
 server.use(indexRoutes)
-server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+// server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 const port = 3333
 server.listen(port, () => { console.log(`SERVER RUNNING ON http://localhost:${port}/`) })
--- source/routes/index.routes.ts
+++ source/routes/index.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 import { categoriesRoutes } from './categories.routes'
 import { specificationRoutes } from './specification.routes'
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from '../bin/server/swagger.json'
 
 const indexRoutes = Router()
 
+indexRoutes.get('/', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 indexRoutes.use('/categories', categoriesRoutes)
 indexRoutes.use('/specifications', specificationRoutes)
 
